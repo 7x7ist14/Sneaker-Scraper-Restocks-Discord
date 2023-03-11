04aed37bd711967022e7426bcbf0e6621be184f2
jakob.aio <103487648+7x7ist14@users.noreply.github.com>
2023-03-11 17:36:59 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,10 @@
-TOKEN = ""
 
-CHANNEL_NAME = ""
+#Input your discord bot token here:
+TOKEN = ''
+
+#Input the name of the discord channel here (channel not server!):
+CHANNEL_NAME = 'scraper'
+
+#if you want to change the command you can do it here:
+COMMAND_PREFIX = "$restocks"
+# $restocks is the standart command but you can change it how you want to. for example: %r
