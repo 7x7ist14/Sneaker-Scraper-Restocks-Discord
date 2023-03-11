19011eb9c2c47f42d7669966dbf4a8ec32c3bc69
jakob.aio <103487648+7x7ist14@users.noreply.github.com>
2023-03-11 17:37:10 +0100

    Update discord_embed.py

--- discord_embed.py
+++ discord_embed.py
@@ -1,6 +1,7 @@
 import discord
+import datetime
 import restocks_main
-from config import TOKEN, CHANNEL_NAME
+from config import TOKEN, CHANNEL_NAME, COMMAND_PREFIX
 from discord.ext import commands
 
 restocks_url = restocks_main.restocks_url
@@ -10,12 +11,16 @@ restocks_title = restocks_main.product_title
 stockx_url = restocks_main.stockx_url
 hypeboost_url = restocks_main.hypeboost_product_url
 sneakit_url = restocks_main.sneakit_product_url
+goat_url = restocks_main.product_goat
 
 if not TOKEN:
-    raise ValueError("The BOt-Token was not included in the config.py file")
+    raise ValueError("The Bot-Token was not included in the config.py file")
 
 if not CHANNEL_NAME:
-    raise ValueError("The Channel-name was not included in the config.py file")
+    raise ValueError("The Channel-Name was not included in the config.py file")
+
+if not COMMAND_PREFIX:
+    raise ValueError("The Command-Prefix was not included in the config.py file")
 
 bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 
@@ -32,11 +37,12 @@ async def on_message(message):
   message_content = message.content.lower()
 
   if message.channel.name == CHANNEL_NAME:
-    if message.content.startswith(f'$restocks'): #(Prefix "$") SKU for scraping
+    if message.content.startswith(COMMAND_PREFIX): #(Prefix "$") SKU for scraping
 
-      if f'$restocks' in message_content:
+      if COMMAND_PREFIX in message_content:
           await message.channel.send("Scraping...")
-          SKU = message_content.replace('$restocks ', '') # everything after prefix is the search (SKU)
+          SKU_raw = message_content.replace(COMMAND_PREFIX, '') # everything after prefix is the search (SKU)
+          SKU = SKU_raw.replace(" ", "")
           restocks_sizes_output = restocks_sizes(SKU)
           restocks_product_output = restocks_url(SKU)
           restocks_image_output = restocks_pic(SKU,restocks_url)
@@ -44,6 +50,7 @@ async def on_message(message):
           stockx_url_output = stockx_url(SKU)
           hypeboost_url_output = hypeboost_url(SKU)
           sneakit_url_output = sneakit_url(SKU)
+          goat_url_output = goat_url(SKU)
 
           embed = discord.Embed(
             title=restocks_title_output,
@@ -63,12 +70,12 @@ async def on_message(message):
             value=restocks_sizes_output
           )
           embed.add_field(
-            name="Open Product on:",
-            value=f"[[StockX]]({stockx_url_output})      " f"[[Sneakit]]({sneakit_url_output})      " f"[[Restocks]]({restocks_product_output})      " f"[[Hypeboost]]({hypeboost_url_output})      ",
-            inline=False
+          name="Open Product on:",
+          value=f"[[StockX]]({stockx_url_output})      " f"[[Sneakit]]({sneakit_url_output})      " f"[[Restocks]]({restocks_product_output})      " f"[[Hypeboost]]({hypeboost_url_output})      " f"[[GOAT]]({goat_url_output})      ",
+          inline=False
           )
           embed.set_footer(
-            text="Developed by Jakob.AIO"
+            text=f"Developed by JakobAIO      |      Hypeboost-Payout      |      {datetime.datetime.now().strftime('%H:%M:%S')}"
           )
 
           await message.channel.send(embed=embed) #sends sizes in discord chat
